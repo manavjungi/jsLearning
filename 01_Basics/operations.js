b4e9f7ebc3bd7816e9dc81dc2fb631fe96ff66c0
manavjungi <110090635+manavjungi@users.noreply.github.com>
2025-06-06 09:40:29 +0000

    new learning done

---
+++ 01_Basics/operations.js
@@ -0,0 +1,16 @@
+let value = 3
+let negValue = -value
+
+console.log(negValue);
+
+let string1 = "hello"
+ let string2 = "manav"
+
+ let string3 = string1 + string2
+ console.log(string3);
+ 
+ console.log("1"+2+2);
+ console.log(2+2+"1");
+ 
+ 
+
