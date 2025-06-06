79b6084c4279d942b14913431d3ae8cd107b4c92
manavjungi <110090635+manavjungi@users.noreply.github.com>
2025-06-06 06:59:30 +0000

    new done

---
+++ 01_Basics/datatypes.js
@@ -0,0 +1,28 @@
+"use strict"; // treates all JS code as newer version
+
+// alert("Hi I Manav") // we are using nodejs not browser so not working 
+
+console.log(3+3)
+
+console.log("Manavvv");
+
+let name = "Manav"
+let age = 19
+let isLoggedIn = false
+
+// number => to power 53 range for storing numbers
+// bigint
+// string => "" use double qoutes only
+// boolean => true or false
+// null => standalone value
+// undefined => 
+// symbol => unique 
+
+
+// object 
+
+console.log(typeof undefined);
+
+
+
+
---
+++ 01_Basics/variable.js
@@ -0,0 +1,11 @@
+const accID= "24bce515"
+let Name = "Manav"
+var learning = "JavaScript"
+city = "Amdavad"
+
+// accID=1234 // Cannot change the value of constant data defined 3
+
+console.log(accID);
+
+console.table([accID,Name,learning,city])
+ 
