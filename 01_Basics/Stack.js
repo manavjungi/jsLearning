87c74cbe7c4157450b5558401b96ae137e0dad6f
manavjungi <110090635+manavjungi@users.noreply.github.com>
2025-06-09 05:03:52 +0000

    Stack Done

---
+++ 01_Basics/Stack.js
@@ -0,0 +1,14 @@
+let cName = "ManavJungi"
+
+let anotherName = cName
+
+anotherName = "joker" 
+
+console.log(cName);
+console.log(anotherName);
+
+
+let user  = {
+    email: "manav.jungi11@gmail.com",
+    upi: "user@upi"
+}
