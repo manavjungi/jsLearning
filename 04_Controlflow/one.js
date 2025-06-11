8e1fe3a1b4df70fd6c02b8651f94ed60725b09bd
manavjungi <110090635+manavjungi@users.noreply.github.com>
2025-06-11 04:50:06 +0000

    done

---
+++ 04_Controlflow/one.js
@@ -0,0 +1,25 @@
+// if 
+const isUserLoggedIn = true
+const temperature = 53
+
+// if(temperature < 50){
+//     console.log("Less than 50");
+    
+// }
+// else{
+//     console.log("temp high");
+// }
+
+const score = 200
+
+// if (score > 100){
+//     const power = "fly"
+//     console.log(`User power : ${power}`);
+// }
+
+ // console.log(`User power : ${power}`); // scope issues
+
+ const balance = 1000
+
+//  if (balance > 500) console.log("yes it greater");
+ 
